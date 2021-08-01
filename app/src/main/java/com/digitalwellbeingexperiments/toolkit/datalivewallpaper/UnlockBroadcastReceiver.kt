549be361b580078d73a2bb6119aba68b8c23f46f
milankomaj <82147606+milankomaj@users.noreply.github.com>
2021-08-01 12:15:20 +0200

    Update UnlockBroadcastReceiver.kt

--- app/src/main/java/com/digitalwellbeingexperiments/toolkit/datalivewallpaper/UnlockBroadcastReceiver.kt
+++ app/src/main/java/com/digitalwellbeingexperiments/toolkit/datalivewallpaper/UnlockBroadcastReceiver.kt
@@ -23,7 +23,7 @@ import java.util.*
 
 object UnlockBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val prefs = getSharedPreferences("com.digitalwellbeingexperiments.toolkit.datalivewallpaper", Context.MODE_PRIVATE)
+        val prefs = getSharedPreferences(context)
         if (intent.action == Intent.ACTION_USER_PRESENT) {
 
 
