0c90ba645d342bbc9c2e5d340f593826ed2b8c71
milankomaj <milankomaj+Github@gmail.com>
2021-09-04 03:22:47 +0200

    @Test

--- app/src/test/java/com/digitalwellbeingexperiments/toolkit/datalivewallpaper/ExampleUnitTest.kt
+++ app/src/test/java/com/digitalwellbeingexperiments/toolkit/datalivewallpaper/ExampleUnitTest.kt
@@ -14,19 +14,16 @@
 
 package com.digitalwellbeingexperiments.toolkit.datalivewallpaper
 
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-import org.junit.Assert.*
+internal class SampleTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+    private val testSample: Sample = Sample()
+
     @Test
-    fun addition_isCorrect() {
-        val expected = 4
-        assertEquals(expected.sum(2, 2))
+    fun testSum() {
+        val expected = 42
+        assertEquals(expected, testSample.sum(40, 2))
     }
 }
